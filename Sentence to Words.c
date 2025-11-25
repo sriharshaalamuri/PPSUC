cfb5c65f32f750162244e5b421b796e407371082
Sri Harsha Alamuri <sriharshaalamuri@gmail.com>
2025-11-25 10:41:22 +0530

    Add files via upload

---
+++ Sentence to Words.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[40];
+    int i;
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        printf("\n");
+        else
+        printf("%c",a[i]);
+    }
+
+    return 0;
+}
